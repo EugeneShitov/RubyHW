7010c44022dd4fcffcbbe2059e917070f2098ea2
Evgeniy Sh <fured.rb@gmail.com>
2022-10-21 03:23:24 +0300

    Completed 50 tasks

---
+++ HW1/46.rb
@@ -0,0 +1,4 @@
+p array = [15, 0, 12, 2, 17, 3, 0, -18]
+p 'Дан целочисленный массив. Найти максимальный отрицательный элемент.'
+p 'array.find_all(&:negative?).max'
+p array.find_all(&:negative?).max
